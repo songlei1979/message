24f01074c1676c3d783c669b24ce8fa1467c6800
Lei <gabriel_sl19798@hotmail.com>
2024-07-08 13:35:35 +1200

    add allowed methods

--- message/settings.py
+++ message/settings.py
@@ -134,6 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:3000',
 # ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
